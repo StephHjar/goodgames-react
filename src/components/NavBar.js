7943cfb0a87ed85c81b2b472f587d5a0e30fca63
Steph Hjartarson <stephanie.hja@gmail.com>
2023-02-26 16:51:06 +0000

    Add CSS styling to navbar and index.html

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,18 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import logo from "../assets/GoodGames-logo.webp";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
-          <img src={logo} alt="GoodGames logo" height="200" />
+          <img src={logo} alt="GoodGames logo" height="100" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
+          <Nav className="ml-auto text-right">
             <Nav.Link>
               <i className="fas fa-home"></i>Home
             </Nav.Link>
